676ed91782343e7e7bc08c40b30322a4135d02bf
eliza <twew02@gmail.com>
2022-11-25 13:30:38 -0500

    create useCountdown hook

---
+++ src/hooks/useCountdown.js
@@ -0,0 +1,28 @@
+import {useState, useEffect} from 'react';
+
+
+const useCountdown = (targetDate) => {
+ const expiryDate = new Date(targetDate).getTime();
+ const currentDate = new Date.getTime();
+
+ const [countdown, setCountdown] = useState(expiryDate - currentDate);
+
+ useEffect (() => {
+  const timeInterval = setInterval(()=>{
+    setCountdown(expiryDate - currentDate);
+  }, 1000);
+  return clearInterval(timeInterval);
+ }, [expiryDate])
+ return getReturnCount(countdown);
+};
+
+const getReturnCount = (countdown) {
+  const days = Math.floor(countdown / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((countdown % (1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+  const mins = Math.floor((countdown % (1000 * 60 * 60))/ (1000 * 60));
+  const secs = Math.floor((countdown % (1000 * 60)) / 1000);
+
+  return [days, hours, mins, secs];
+}
+
+export {useCountdown};
